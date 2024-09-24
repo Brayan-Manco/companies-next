3850b869493ba40c2cb004db4cdd734b4274d383
Brayan <manco300mz@gmail.com>
2024-09-24 04:39:08 -0500

    setting eslint to production

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+const isProd = process.env.NODE_ENV === 'production'
+
+const nextConfig = {
+    eslint: {
+        // Deshabilitar ESLint en producción
+        ignoreDuringBuilds: isProd,
+      },
+};
 
 export default nextConfig;
